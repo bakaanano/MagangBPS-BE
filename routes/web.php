c525d799478ac3e446d190dc1868bbbda7a4503b
TaufiqAndriono <taufiqandriono688@gmail.com>
2025-08-27 08:49:24 +0700

    second commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,46 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+// use App\Http\Controllers\SupabaseController;
+use Illuminate\Support\Facades\DB;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/mitra', function () {
+    try {
+        // Ambil semua data dari tabel mitra
+        $mitra = DB::table('mitra')->get();
+
+        return response()->json([
+            'success' => true,
+            'data' => $mitra
+        ]);
+    } catch (\Throwable $e) {
+        return response()->json([
+            'success' => false,
+            'error' => $e->getMessage(),
+        ], 500);
+    }
+});
+
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return Inertia::render('Welcome');
+})->name('home');
+
+Route::get('dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
